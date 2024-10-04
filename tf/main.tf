80275d3f81d9dc596004455eb3ad31381f518742
bennyi <stonecold344@gmail.com>
2024-10-04 17:15:07 +0300

    commit

--- tf/main.tf
+++ tf/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "polybot_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Update this to your current IP if necessary
   }
 
   ingress {
