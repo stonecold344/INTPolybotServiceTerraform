300594b317bb1b5f536df68c52407858e59374ca
bennyi <stonecold344@gmail.com>
2024-10-19 18:38:57 +0300

    Test us eu

--- tf/main.tf
+++ tf/main.tf
@@ -459,7 +459,7 @@ resource "aws_instance" "polybot_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -471,7 +471,7 @@ resource "aws_instance" "polybot_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -490,7 +490,7 @@ resource "aws_instance" "polybot_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -504,7 +504,7 @@ resource "aws_instance" "polybot_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -574,7 +574,7 @@ resource "aws_instance" "yolo5_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -586,7 +586,7 @@ resource "aws_instance" "yolo5_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
@@ -612,7 +612,7 @@ resource "aws_instance" "yolo5_instance" {
     connection {
       type        = "ssh"
       user        = "ubuntu"
-      private_key = file("./tf/aws-bennyi.pem")
+      private_key = var.aws_private_key
       host        = self.public_ip
     }
   }
