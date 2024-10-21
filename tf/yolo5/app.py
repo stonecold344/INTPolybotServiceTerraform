76d3a5c33dd02ca27ec171c4a0da58c054dcc0d8
bennyi <stonecold344@gmail.com>
2024-10-21 17:47:01 +0300

    Test us eu

--- tf/yolo5/app.py
+++ tf/yolo5/app.py
@@ -36,7 +36,7 @@ def get_secret(secret_id):
         raise e
 
 # Retrieve the Telegram token
-SECRET_ID = "Telegram-Secret-Bennyi24"
+SECRET_ID = "Telegram-Secret-Bennyi25"
 secrets = get_secret(SECRET_ID)
 TELEGRAM_TOKEN = secrets.get("Telegram-Secret-Bennyi")
 
